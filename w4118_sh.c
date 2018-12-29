82efe3f34264ebe980c1eb84b085930b6e709bcb
Matthew Chan <matthew.a.chan@gmail.com>
2018-12-28 18:47:44 -0800

    Add cd builtin

--- w4118_sh.c
+++ w4118_sh.c
@@ -8,22 +8,29 @@
 /* Split up an input string by space delimiters. Each token of this string
  * will form the array of arguments which we will pass to the exec syscall.
  */
-void tokenize(char **buf, char *input)
+int tokenize(char **buf, char *input)
 {
+	int retval;
 	char *token = strtok(input, " ");
 	int cntr = 0;
-	while (token) {
+
+	while (token && cntr < MAX_ARGS) {
 		buf[cntr++] = token;
 		token = strtok(NULL, " ");
 	}
+	retval = cntr;
 
 	while (cntr < MAX_ARGS)
 		buf[cntr++] = NULL;
+
+	return retval;
 }
 
 int main(int argc, char **argv)
 {
+	int ntokens;
 	ssize_t nread;
+	int nused = 0;
 	char **buf = malloc(MAX_ARGS * sizeof(char *));
 	char *input = NULL;
 	size_t len = 0;
@@ -34,23 +41,35 @@ int main(int argc, char **argv)
 
 	while (1) {
 		printf("$");
-		nread = getline(&input, &len, std_in);
+		
+		if ((nread = getline(&input, &len, std_in)) == -1) {
+			perror("getline");
+			exit(EXIT_FAILURE);
+		}
 		/* Remove newline ending */
 		input[nread - 1] = '\0';
 
-		tokenize(buf, input);
-		if (strcmp(buf[0], "exit") == 0 && nread == 5)
+		ntokens = tokenize(buf, input);
+		if (ntokens > nused)
+			nused = ntokens;
+
+		if (strcmp(buf[0], "exit") == 0 && ntokens == 1)
 			break;
+		else if (strcmp(buf[0], "cd") == 0 && ntokens == 2) {
+			chdir(buf[1]);
+			continue;
+		}
+
 
 		if (fork() == 0)
 			execv(buf[0], buf);
 		wait(NULL);
+
 	}
 
 	free(input);
-	for (int i = 0; i < MAX_ARGS; ++i)
-		free(buf[i]);
 	free(buf);
+	fclose(std_in);
 
 	return 0;
 }
